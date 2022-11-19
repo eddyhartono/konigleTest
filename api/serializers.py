0290c71553c2744b1f26e0fb7568f2ce12913705
Eddy Hartono <118254967+eddyhartono@users.noreply.github.com>
2022-11-20 03:39:06 +0700

    Delete serializers.py

--- api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import emailList
-
-class EmailSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = emailList
-        fields = "__all__"
-        ordering = ['-date_action']
