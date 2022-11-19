d8878a451f48e212c4152bc43f8f5ecb73e8dcc8
Eddy Hartono <118254967+eddyhartono@users.noreply.github.com>
2022-11-20 03:36:38 +0700

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,3 @@
-from django.forms import ValidationError
-from django.core.exceptions import ObjectDoesNotExist
-
 from rest_framework import serializers
 from .models import emailList
 
@@ -9,11 +6,3 @@ class EmailSerializer(serializers.ModelSerializer):
         model = emailList
         fields = "__all__"
         ordering = ['-date_action']
-
-    def validate_email(self, value):
-        try:
-            check_email = emailList.objects.get(email__exact=value)
-            if check_email :
-                raise ValidationError('Email is already Signed Up')
-        except ObjectDoesNotExist:
-            return value
